8ee534102c8029182be8b311ae86ace0c8e9a8fb
Dave Campbell <dave@digetl.co.uk>
2020-05-12 20:06:02 +0100

    setup basic html for loading currency

--- src/app.js
+++ src/app.js
@@ -4,10 +4,25 @@ document.addEventListener('DOMContentLoaded', () => {
   new Vue({
     el: "#app",
     data: {
+        currencies: [],
       
     },
+    computed:{
+        convertedCurrency: function(){
+
+        },
+        convertedCurrencyName: function(){
+
+        }
+    },
+    mounted(){
+      this.fetchCurrencies();
+    },
+
     methods: {
-        
+        fetchCurrencies: function(){
+            ("https://api.exchangeratesapi.io/latest")
+        },
        
     }
     
